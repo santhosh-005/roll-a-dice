e920e1c92634a2971124fcd3cccb0dbb8f80b596
santhoshkumar v <138782397+santhosh-005@users.noreply.github.com>
2023-07-06 01:15:27 -1200

    Add files via upload

---
+++ DICE ROLER.py
@@ -0,0 +1,13 @@
+import random as SK
+print("WELCOME TO DICE ROLLER")
+while True:
+    list1=['1','2','3','4','5','6']
+    A=input("PRESS ENTER TO ROLL A DIE:")
+    if A=="" :
+        print("YOUR CHOISE",SK.choice(list1))
+    else:
+        print("INVALID INPUT")
+        
+
+        
+        
